878879e1432c4fe78faaf44490c2dcf36de30cf4
Fizzy1995 <jezzmond2012@gmail.com>
2026-02-18 10:19:33 +1030

    tried to make fade anim

--- Scripts/EnterGame.cs
+++ Scripts/EnterGame.cs
@@ -3,7 +3,20 @@ using System;
 
 public partial class EnterGame : Node2D
 {
-	private void OndialogPlayerDialogEnded() 
+	private AnimationPlayer _animationplayer;
+	
+	public override void _Ready()
+	{
+		_animationplayer = GetNode<AnimationPlayer>("FadeToBlack");
+
+		_animationplayer.AnimationFinished += OnFadeAnimationFinished;
+	}
+	private void OnDialogPlayerDialogEnded() 
+	{
+		_animationplayer.Play("FadeToBlack");
+	}
+
+	private void OnFadeAnimationFinished(FadeToBlack)
 	{
 		GetTree().ChangeSceneToFile("res://Scenes/Gameplay.tscn");
 	}
