c7e94c4ccc495923015c4312673b627162fab59c
Fizzy1995 <jezzmond2012@gmail.com>
2026-02-20 15:00:53 +1030

    tried adding audio

--- Scripts/EnterGame.cs
+++ Scripts/EnterGame.cs
@@ -4,19 +4,22 @@ using System;
 public partial class EnterGame : Node2D
 {
 	private AnimationPlayer _animationplayer;
-	
+	private AudioStreamPlayer TransitionSound;
 	public override void _Ready()
 	{
 		_animationplayer = GetNode<AnimationPlayer>("FadeToBlack");
+
+		TransitionSound = GetNode<AudioStreamPlayer>("TransitionSound");
 
 		_animationplayer.AnimationFinished += OnFadeAnimationFinished;
 	}
 	private void OnDialogPlayerDialogEnded() 
 	{
+		TransitionSound.Play();
 		_animationplayer.Play("FadeToBlack");
 	}
 
-	private void OnFadeAnimationFinished(FadeToBlack)
+	private void OnFadeAnimationFinished(StringName animName)
 	{
 		GetTree().ChangeSceneToFile("res://Scenes/Gameplay.tscn");
 	}
--- Scripts/PlayButton.cs
+++ Scripts/PlayButton.cs
@@ -2,8 +2,15 @@ using Godot;
 
 public partial class PlayButton : Button
 {
+	private AudioStreamPlayer ButtonPressedSound;
+
+	public override void _Ready()
+	{
+		ButtonPressedSound = GetNode<AudioStreamPlayer>("ButtonPressedSound");
+	}
 	public override void _Pressed()
 	{
+		ButtonPressedSound.Play();
 		GetTree().ChangeSceneToFile("res://Scenes/Interlude.tscn");
 	}
 }
