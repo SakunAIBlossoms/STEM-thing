76d33898a98b5c44c58399a6b301371e27df330c
Fizzy1995 <jezzmond2012@gmail.com>
2026-04-01 09:37:29 +1030

    fixed sound

--- Scripts/BackButton.cs
+++ Scripts/BackButton.cs
@@ -1,18 +1,33 @@
 using Godot;
-using System;
+using System.Collections.Generic;
 
 public partial class BackButton : Button
 {
-    private AudioStreamPlayer ButtonPressedSound;
-
+    Dictionary<string, AudioStreamPlayer> Sounds;
     public override void _Ready()
     {
-        ButtonPressedSound = GetNode("/root/Sounds/ButtonPressedSound") as AudioStreamPlayer;
-        
+        Sounds = new Dictionary<string, AudioStreamPlayer>
+        {
+            { "Pressed", GetNode("/root/Sounds/ButtonPressed") as AudioStreamPlayer },
+            {"Hovered", GetNode("/root/Sounds/ButtonHover") as AudioStreamPlayer},
+            {"HoverStopped", GetNode("/root/Sounds/ButtonHoverStopped") as AudioStreamPlayer}
+        };
     }
     public override void _Pressed()
     {
-        ButtonPressedSound.Play();
+        Sounds["Pressed"].Play();
         GetTree().ChangeSceneToFile("res://Scenes/MainMenu.tscn");
+    }	
+    public void OnMouseEntered()
+    {
+        Sounds["Hovered"].Stop();
+        Sounds["Hovered"].PitchScale = 0.6f;
+        Sounds["Hovered"].Play();
+    }
+    public void OnMouseExited()
+    {
+        Sounds["HoverStopped"].Stop();
+        Sounds["HoverStopped"].PitchScale = 0.6f;
+        Sounds["HoverStopped"].Play();
     }
 }
