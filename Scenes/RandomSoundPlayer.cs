46b391660214881f299d125afaa280d28ed1cc0c
Fizzy1995 <jezzmond2012@gmail.com>
2026-04-01 10:20:26 +1030

    tried randomly moving audio

--- Scenes/RandomSoundPlayer.cs
+++ Scenes/RandomSoundPlayer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Numerics;
 using Godot;
 
 public partial class RandomSoundPlayer : Node
@@ -6,17 +7,19 @@ public partial class RandomSoundPlayer : Node
     [Export]
     public AudioStream[] Sounds;
     private Timer timer;
-    private AudioStreamPlayer audio;
+    private AudioStreamPlayer3D audio;
 
     private float minDelay = 20f; // seconds
     private float maxDelay = 60f;
+    private float minPos = -20f;
+    private float maxPos = 20f;
 
     public override void _Ready()
     {
         GD.Randomize();
-
+        
         timer = GetNode<Timer>("Timer");
-        audio = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
+        audio = GetNode<AudioStreamPlayer3D>("AudioStreamPlayer3D");
 
         timer.Timeout += OnTimerTimeout;
 
@@ -25,6 +28,7 @@ public partial class RandomSoundPlayer : Node
 
     private void OnTimerTimeout()
     {
+        MoveRandomLocation();
         PlayRandomSound();
         StartRandomTimer();
     }
@@ -34,6 +38,13 @@ public partial class RandomSoundPlayer : Node
         int index = GD.RandRange(0, Sounds.Length - 1);
         audio.Stream = Sounds[index];
         audio.Play();
+    }
+
+    private void MoveRandomLocation()
+    {
+        float randomPosValue = (float)GD.RandRange(minPos, maxPos)
+        Godot.Vector3 randomPos = randomPosValue, randomPosValue, randomPosValue;
+        audio.Position = randomPos;
     }
     private void StartRandomTimer()
     {
